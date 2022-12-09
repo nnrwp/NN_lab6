6ab65c8695851b68a352dd28c6ce1cc4e4e91d8c
nnrwp <rawipa_s@cmu.ac.th>
2022-12-09 15:25:13 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i, even=0, odd=0;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> i;
+
+        if(i == 0){
+            break;
+        }
+
+        if (i%2 == 0){
+            even += 1;
+        }else{
+            odd += 1;
+        }
+    }
+    
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd << "\n";
     return 0;
 }
